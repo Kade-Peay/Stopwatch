473d328b252addc997d2fca32d498e5922a56aef
kadepeay <kadepeay@gmail.com>
2020-09-25 00:44:22 -0600

    able to add time at multiple intervals

---
+++ main.py
@@ -0,0 +1,28 @@
+# the goal is to time how much time spent on a project and add it all up
+# so I need it to keep track in seconds, then convert to hours or minutes
+# I also need it to keep track of multiple sessions
+
+import time, sys
+
+def menu(x):
+    if(x == 'n'):
+        totalTime = 0
+        try:
+            while True:
+                time.sleep(1)
+                totalTime += 1
+                print(totalTime, end='\r', flush=True)
+        except KeyboardInterrupt:
+            print('total time was : ', totalTime)
+            with open('totalTimeFile.txt', 'a') as f:
+                f.write(str(totalTime) + '\n')
+    if(x == d): 
+        pass
+        
+
+if __name__ == "__main__":
+    print('n for new, d for done with current project: ')
+    choice = input("choose: ")
+    menu(choice)
+
+# __main__()
